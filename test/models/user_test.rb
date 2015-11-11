058ca09d7224385740930f736bcc77f8d5f455d2
Don Deckard <don_deckard@yahoo.com>
2015-11-11 10:47:37 -0500

    Working on tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "don't save user without name" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "don't save user with same name" do
+    user = User.new
+    assert_not user.save
+  end
+
 end
