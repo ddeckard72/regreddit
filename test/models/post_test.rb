69351c9c0b7683e8e3bce02ca1a359426e1e68c3
Don Deckard <don_deckard@yahoo.com>
2015-11-11 11:03:19 -0500

    hope it's fixed

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-	test "don't save an empty post"
-    post = Post.new
-    assert_not post.save
+	test "don't save an empty post" do
+    	post = Post.new
+    	assert_not post.save
 	end
 end
