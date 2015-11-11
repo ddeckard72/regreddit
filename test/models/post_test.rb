7a1ce14c39c5f6657204bf288a4fe11081323967
Don Deckard <don_deckard@yahoo.com>
2015-11-11 11:13:31 -0500

    stuff

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class PostTest < ActiveSupport::TestCase
 	test "comment without a user" do
     	post = Post.new
-    	assert_not post.user.empty?
+    	assert post.user.empty?
 	end
 end
