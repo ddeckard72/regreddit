40d5ac756296322ab5f0857e45f29550d8bb8b72
Don Deckard <don_deckard@yahoo.com>
2015-11-11 10:55:34 -0500

    added post test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "don't save an empty link"
+    post = Post.new
+    assert_not Post.save
+	end
 end
